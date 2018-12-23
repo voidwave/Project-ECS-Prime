9568d6be908066f5f89853a13eff74e114fd0e61
voidwave <majed@voidwave.com>
2018-12-23 16:20:19 +0300

    Initial commit

---
+++ ECSPrime/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using UnityEngine.Rendering;
+
+public static class GameManager
+{
+    private static Mesh PlayerMesh = (Resources.Load("Prefabs/Player") as GameObject).GetComponent<MeshFilter>().sharedMesh;
+    private static Material PlayerMaterial = (Resources.Load("Prefabs/Player") as GameObject).GetComponent<MeshRenderer>().sharedMaterial;
+
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void Initilize()
+    {
+        Debug.Log("GameManager Initilize();");
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        var playerArchetype = entityManager.CreateArchetype(
+            typeof(LocalToWorld),
+            typeof(Position),
+            typeof(Rotation),
+            typeof(MoveSpeed),
+            typeof(MeshInstanceRenderer)
+        );
+
+        var player = entityManager.CreateEntity(playerArchetype);
+
+        entityManager.SetSharedComponentData(player, new MeshInstanceRenderer
+        {
+            mesh = PlayerMesh,
+            material = PlayerMaterial,
+            castShadows = ShadowCastingMode.On
+        });
+
+        entityManager.SetComponentData(player, new MoveSpeed{ Value = 1 });
+
+    }
+
+}
---
+++ ECSPrime/Assets/Scripts/PlayerMovementSystem.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+
+public class PlayerMovementSystem : JobComponentSystem
+{
+    [ComputeJobOptimization]
+    struct PlayerMovementJob : IJobProcessComponentData<Position, Rotation, MoveSpeed>
+    {
+        public float Horizontal;
+        public float Vertical;
+        public float deltaTime;
+
+        public void Execute(ref Position position, ref Rotation rotation, [ReadOnly] ref MoveSpeed speed)
+        {
+
+            if (Horizontal == 0 && Vertical == 0)
+                return;
+
+            float3 value = position.Value;
+            float3 dir = new float3(Horizontal, 0, Vertical);
+            dir = math.normalize(dir);   
+            rotation.Value = Quaternion.LookRotation(dir);
+            value += deltaTime * speed.Value * math.forward(rotation.Value);
+            position.Value = value;
+
+        }
+
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        PlayerMovementJob moveJob = new PlayerMovementJob
+        {
+            Horizontal = Input.GetAxis("Horizontal"),
+            Vertical = Input.GetAxis("Vertical"),
+            deltaTime = Time.deltaTime
+        };
+
+
+        JobHandle moveHandle = moveJob.Schedule(this, inputDeps);
+
+        return moveHandle;
+    }
+
+}
